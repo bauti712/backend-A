1a72b4897d27085925162635be5d58758bc5a09b
Bautista Cola <i.cola@alumno.etec.um.edu.ar>
2023-11-13 21:54:32 -0300

    cnexion desde backend

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,21 +1,29 @@
 import express from "express";
 import cors from "cors";
 import { AppRoutes } from "./routes";
+import { appDataSource } from "../db/conection";
 
 class Server {
   private readonly app = express();
   private readonly port = 8080;
-  public start = () => {
-    this.middlewares();
+  public start = async() => {
+    await this.middlewares();
     this.app.use(AppRoutes.routes)
     this.app.listen(this.port, () => {
       console.log("server running in port " + this.port);
     });
   };
-  private middlewares = () => {
+  private middlewares = async () => {
     this.app.use(cors());
     this.app.use(express.json());
-
+    try {
+      await appDataSource.initialize()
+      console.log ("conexion establecida correctamente ")
+    } catch (error) {
+      console.log(error)
+      
+    }
+    
   };
 }
 
