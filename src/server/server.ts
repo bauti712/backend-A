5d0d60210eb719825a68f86a85c39adcb63621e1
Bautista Cola <i.cola@alumno.etec.um.edu.ar>
2023-11-27 14:22:14 -0300

    arreglo imagenes

--- src/server/server.ts
+++ src/server/server.ts
@@ -26,16 +26,16 @@ class Server {
         const productRepository = connection.getRepository(Product);
         const existingProducts = await productRepository.find();
         if (existingProducts.length === 0) {
-          const producto1 = new Product("Mini 2", 80, "https://images.unsplash.com/photo-1514228742587-6b1558fcca3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80");
-          const producto2 = new Product("dji air 2", 120, "https://images.unsplash.com/photo-1570784332176-fdd73da66f03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80");
-          const producto3 = new Product("dji spark", 450, "https://images.unsplash.com/photo-1570784332176-fdd73da66f03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80");
-          const producto4 = new Product("air", 80, "https://images.unsplash.com/photo-1570784332176-fdd73da66f03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80");
-          const producto5 = new Product("dji inspire",25, "https://images.unsplash.com/photo-1570784332176-fdd73da66f03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80");
+          const producto1 = new Product("Mini 2", 80, "https://images.unsplash.com/photo-1619892447950-72edfbc8427e?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+          const producto2 = new Product("dji air 2", 120, "https://images.unsplash.com/photo-1631052941794-2a6e26d4ac17?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+          const producto3 = new Product("dji spark", 450, "https://images.unsplash.com/photo-1583824904188-f2f792acd329?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+          const producto4 = new Product("air", 80, "https://images.unsplash.com/photo-1513082033533-d5de90571ceb?q=80&w=2668&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+          const producto5 = new Product("dji inspire",25, "https://images.unsplash.com/photo-1456615913800-c33540eac399?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
 
           await productRepository.save([producto1, producto2, producto3, producto4, producto5]);
       }
 
-        console.log("Connection established successfully");
+        console.log("Connexion funcionando");
       }).catch((error) => {
         console.log(error);
       });
